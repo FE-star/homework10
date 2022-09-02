8760e2c794e25c1f605d39a9ecf8a98fe0df4bc4
haoming <435203093@qq.com>
2022-09-03 02:32:12 +0800

    feat: ssr

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 // TODO 监听3000端口，便于执行test
+import express from 'express'
+import { createSSRApp } from 'vue'
+import { renderToString } from 'vue/server-renderer'
+
+const server = express()
+
+server.get('/', (req, res) => {
+  const app = createSSRApp({
+    data: () => ({ count: 1 }),
+    template: `<div>Vue SSR Example</div>`
+  })
+
+  renderToString(app).then((html) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Vue SSR Example</title>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+      </body>
+    </html>
+    `)
+  })
+})
+
+server.listen(3000, () => {
+  console.log('ready')
+})
