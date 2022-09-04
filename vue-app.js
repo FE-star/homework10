e73b0fbe86f5ed3c5932db3ddd05291cf3cc7ab6
blake.li <blake.li@qunar.com>
2022-09-04 23:43:18 +0800

    submit homework

---
+++ vue-app.js
@@ -0,0 +1,11 @@
+
+import { createSSRApp } from 'vue';
+
+export function createApp() {
+    return createSSRApp({
+        data: () => ({
+            text: 'Vue SSR Example'
+        }),
+        template: `<div>{{text}}</div>`
+    });
+};
