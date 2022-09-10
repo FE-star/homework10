f92174d266ac933ad90665fb19caecad3e44ba6b
Leopen <790480953@qq.com>
2022-09-10 15:10:25 +0800

    feat: 实现基于 vue 的简单 ssr app

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// TODO 监听3000端口，便于执行test
+import express from 'express'
+import { app } from './app.js'
+import { renderToString } from 'vue/server-renderer'
+
+const server = express()
+server.use(express.static('.'))
+
+server.get('/', (req, res) => {
+  renderToString(app).then((html) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Vue SSR Example</title>
+        <script type="importmap">
+        {
+          "imports": {
+            "vue": "/node_modules/vue/dist/vue.esm-browser.js"
+          }
+        }
+        </script>
+        <script type="module" src="/client.js"></script>
+        
+      </head>
+      <body>
+        <div id="app">${html}</div>
+      </body>
+    </html>
+    `)
+  })
+})
+
+server.listen(3000, () => {
+  console.log('ready')
+})
