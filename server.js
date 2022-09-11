aa492475901f0f9d2a81a6b57e37f46ebedd296c
Chris Wang <chris.wang@tcam.co>
2022-09-12 01:54:14 +1000

    Update server

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 // TODO 监听3000端口，便于执行test
+import express from 'express';
+import { Vue, createApp } from 'vue';
+const server = express();
+
+server.get('*', (req, res) => {
+  // const app = new Vue({
+  //   data: {
+  //     array: ['vue', 'java', 'python', 'javascript'],
+  //   },
+  //   template: `<div id="app"><div>Vue SSR Example</div></div>`,
+  // });
+
+  res.send(
+    '<!DOCTYPE html><html lang="en"><head><title>Vue Js – SSR SAMPLE</title></head><body>html</body></html>'
+  );
+});
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
