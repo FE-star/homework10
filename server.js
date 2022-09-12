4c15096805ca4bf91915d544c961a906df83d0e8
zhudui <1209300584@qq.com>
2022-09-12 21:46:00 +0800

    feat: 完成test

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// TODO 监听3000端口，便于执行test
+import express from 'express'
+import { createSSRApp } from "vue";
+import { renderToString } from "vue/server-renderer";
+
+async function createVueSSRApp() {
+  const app = createSSRApp({
+    data: () => ({ title: "Vue SSR Example" }),
+    template: 
+`<body>
+  <div id="app"><div>{{ title }}</div></div>
+</body>`,
+  });
+
+  return renderToString(app);
+}
+
+const server = express()
+
+server.get('/', (req,res) => {
+    createVueSSRApp().then((html) => {
+        res.end(html);
+      });
+})
+
+server.listen(3000, () => {
+  console.log(`Vue SSR Example Ready`);
+});
